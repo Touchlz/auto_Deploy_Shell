e66558d96dff44c49ba12e8d9a07fb6c5ae0224e
Touchlz <Touchl2018@outlook.com>
2019-11-19 19:06:50 +0800

    modify auto_Deploy_LNMP.sh : improve and perfect

--- auto_Deploy_LNMP.sh
+++ auto_Deploy_LNMP.sh
@@ -10,12 +10,14 @@ yumIns gcc;yumIns pcre-devel;yumIns openssl-devel;yumIns mariadb;yumIns mariadb-
 read -p "请指定nginx源码包路径:" path
 tar -xf $path -C /root/ &> /dev/null
 cd ${path%.tar.gz}
-id nginx
+id nginx &> /dev/null
 if [ $? -ne 0 ];then
 	useradd -s /sbin/nologin nginx &> /dev/null
 fi
 ./configure --prefix=/usr/local/nginx --user=nginx --group=nginx --with-http_ssl_module --with-stream --with-http_stub_status_module &> /dev/null
-make && make install &> /dev/null
+( make &> /dev/null ) && (make install &> /dev/null)
+netstat -ntulp | grep -q nginx
+[ $? -eq 0 ] && nginx -s stop
 cp /usr/local/nginx/sbin/nginx /usr/bin/nginx
 nginx
 systemctl restart mariadb &> /dev/null
